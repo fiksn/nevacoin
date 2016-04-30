8223849a500cb03b3109c32b68be3f6b86b88704
kristian <kristian.ilijevski@gmail.com>
2016-04-30 12:26:22 +0200

    add checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -28,8 +28,10 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
          ( 0,   uint256("0x00000e4b9ec4fef5b26bc05a8a50b999fd312ee6f8bf324371ffd790c2961988") )
+         ( 50000,   uint256("0x00000000000343953257931cfa5238b192d183d3369521853f2023cb3a2f44c3") )
 
     ;
+
 
     // TestNet has no checkpoints
     static MapCheckpoints mapCheckpointsTestnet;
